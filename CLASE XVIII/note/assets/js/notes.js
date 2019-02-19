7a5f009e825092024de4b82fe412fa5cc57ce21f
deivixx <d.alcaraz.perez@gmail.com>
2019-02-19 12:13:36 +0100

    Bootstrap and Jquery via webpack

--- CLASE XVIII/note/assets/js/notes.js
+++ CLASE XVIII/note/assets/js/notes.js
@@ -1,7 +1,5 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require('./home.js');
+
 alert("aquí podrás ver todas las notas");
-console.log("jajajaq");
+console.log($('div'));
+
