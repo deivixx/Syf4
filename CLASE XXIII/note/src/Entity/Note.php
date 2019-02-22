b586a8e09b06f1746869aa4ed508190dee8fdd60
deivixx <d.alcaraz.perez@gmail.com>
2019-02-22 14:47:28 +0100

    CLASE XXIII - init

---
+++ CLASE XXIII/note/src/Entity/Note.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+class Note {
+
+    /**
+    * @Assert\NotBlank
+    * @Assert\Length(max=10, maxMessage="Título demasiado largo")
+    */
+    protected $title;
+    
+    /**
+    * @Assert\NotBlank
+    * @Assert\Length(max=255, maxMessage="Nota demasiado larga", min=4, minMessage="Nota demasiado corta")
+    */    
+    protected $note;
+    
+    /**
+     * @Assert\NotBlank
+     * @Assert\Type("\DateTime")
+     */
+    protected $dueDate;
+
+    function getTitle() {
+        return $this->title;
+    }
+
+    function getNote() {
+        return $this->note;
+    }
+
+    function setTitle($title) {
+        $this->title = $title;
+    }
+
+    function setNote($note) {
+        $this->note = $note;
+    }
+    
+    public function getDueDate() {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTime $dueDate = null) {
+        $this->dueDate = $dueDate;
+    }
+
+}
---
+++ CLASE XXIII/note/src/Form/NoteType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+
+class NoteType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+                ->add('title', TextType::class, ['label' => 'Título','help' => 'Escribe el título de la nota'])
+                ->add('note', TextType::class, ['label' => 'Nota', 'help' => 'Escribe la nota'])
+                ->add('dueDate', DateType::class, ['label' => 'Fecha vencimiento', 'help' => 'Fecha en la que caduca'])
+                ->add('save', SubmitType::class, ['label' => 'Guardar Nota'])
+        ;
+    }
+
+}
