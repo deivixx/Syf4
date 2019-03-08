6e59fe7fe10c6046a908d0764bb170c04fde440e
deivixx <d.alcaraz.perez@gmail.com>
2019-03-08 10:23:24 +0100

    CLASE XXXII

---
+++ CLASE XXXII/note/src/Entity/NoteItem.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NoteItemRepository")
+ */
+class NoteItem
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=64)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dueDate;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Note", inversedBy="noteItems")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $note;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?\DateTimeInterface
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(?\DateTimeInterface $dueDate): self
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    public function getNote(): ?Note
+    {
+        return $this->note;
+    }
+
+    public function setNote(?Note $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
